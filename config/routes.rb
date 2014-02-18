7c603ef50534b522ca920c24b046cc2c23a8f299
Scot Dalton <scotdalton@gmail.com>
2014-02-18 14:29:24 -0500

    Enable 'dots' in usernames by relaxing the constraint on :id in the routes per http://guides.rubyonrails.org/routing.html#specifying-constraints

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Login::Application.routes.draw do
   use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'users' }
   devise_scope :user do
-    get 'users/:provider/:id(/:institute)', to: 'users#show', as: 'user', constraints: { provider: providers }
+    get 'users/:provider/:id(/:institute)', to: 'users#show', as: 'user',
+      constraints: { provider: providers, id: /[^\/]+/ }
     get 'logout', to: 'devise/sessions#destroy', as: :logout
     get 'login(/:institute)', to: 'devise/sessions#new', as: :login
     get 'api/v1/user', to: 'users#api', defaults: { format: :json }
