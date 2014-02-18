ba48022246e45f819e2525bfe20faad3086ca457
Scot Dalton <scotdalton@gmail.com>
2014-02-18 15:03:22 -0500

    Read secret token from an environment variable, since we definitely don't want it in version control. That's wicked insecure. This vulnerabilty was caught by brakeman. Thanks, brakeman!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Login::Application.config.secret_key_base = '1727601e1d27973aecc36799c1542e8db196a383bde75c838553fdab68220068dea9961ee15e7b5ef39a7e3f74be179f9660fc209f157764a4bb13fe3b175dd4'
+Login::Application.config.secret_key_base = ENV['LOGIN_SECRET_TOKEN']
