2f1e524eb477f51133be6b01c8efe7fddc02cba9
Scot Dalton <scotdalton@gmail.com>
2014-03-04 16:23:36 -0500

    Dry up step definitions using LoginFeatures helpers.

--- features/step_definitions/logging_in_steps.rb
+++ features/step_definitions/logging_in_steps.rb
@@ -1,100 +1,71 @@
-When(/^I am at NYU in New York$/) do
-  @institute = 'nyu'
+Given(/^I am off campus$/) do
+  ENV['REMOTE_ADDR'] = '127.0.0.1'
 end
 
-When(/^I am at NYU in Abu Dhabi$/) do
-  @institute = 'nyuad'
-end
-
-When(/^I am at NYU in Shanghai$/) do
-  @institute = 'nyush'
-end
-
-When(/^I am at the New School$/) do
-  @institute = 'ns'
-end
-
-When(/^I am at Cooper Union$/) do
-  @institute = 'cu'
-end
-
-When(/^I am at NYSID$/) do
-  @institute = 'nysid'
+Given(/^I am at (.+)$/) do |location|
+  ENV['REMOTE_ADDR'] = ip_for_location(location)
 end
 
 When(/^I want to login$/) do
-  visit "login/#{@institute}"
+  visit login_path
 end
 
-Then(/^I should see the NYU torch login button$/) do
-  expect(page).to have_content 'Login with an NYU NetID'
-  expect(page).to have_css '#shibboleth .btn'
+When(/^I want to login with (.+)$/) do |account|
+  visit login_path
 end
 
-Then(/^I should see an option to login with a New School account$/) do
-  expect(page).to have_content 'New School Libraries'
-  expect(page).to have_css '.ns.alt-login'
+When(/^I want to login to (.+)$/) do |location|
+  visit login_path(institute_for_location(location))
 end
 
-Then(/^I should see an option to login with a Cooper Union account$/) do
-  expect(page).to have_content 'The Cooper Union Library'
-  expect(page).to have_css '.cu.alt-login'
+Then(/^I should (not )?see the NYU torch login button$/) do |negator|
+  expectations_for_page(page, negator, *nyu_login_matchers)
 end
 
-Then(/^I should see an option to login with a NYSID account$/) do
-  expect(page).to have_content 'New York School of Interior Design'
-  expect(page).to have_css '.nysid.alt-login'
+Then(/^I should (not )?be able to login with a New School account$/) do |negator|
+  expectations_for_page(page, negator, *ns_login_matchers)
 end
 
-Then(/^I should see an option to login with a Twitter account$/) do
-  expect(page).to have_content 'Twitter'
-  expect(page).to have_css '.twitter.alt-login'
+Then(/^I should (not )?be able to login with a Cooper Union account$/) do |negator|
+  expectations_for_page(page, negator, *cu_login_matchers)
 end
 
-Then(/^I should see an option to login with a Facebook account$/) do
-  expect(page).to have_content 'Facebook'
-  expect(page).to have_css '.facebook.alt-login'
+Then(/^I should (not )?be able to login with a NYSID account$/) do |negator|
+  expectations_for_page(page, negator, *nysid_login_matchers)
 end
 
-Then(/^I should be able to login with a New School account$/) do
-  expect(page).to have_content 'Login with a New School NetID'
-  expect(page).to have_content 'Enter your NetID Username'
-  expect(page).to have_content 'Enter your NetID Password'
+Then(/^I should (not )?see an option to login with an NYU account$/) do |negator|
+  expectations_for_page(page, negator, *nyu_option_matchers)
 end
 
-Then(/^I should see an option to login with an NYU account$/) do
-  expect(page).to have_content 'NYU Libraries'
-  expect(page).to have_css '.nyu.alt-login'
+Then(/^I should (not )?see an option to login with a New School account$/) do |negator|
+  expectations_for_page(page, negator, *ns_option_matchers)
 end
 
-Then(/^I should not see an option to login with a Twitter account$/) do
-  expect(page).not_to have_content 'Twitter'
-  expect(page).not_to have_css '.twitter.alt-login'
+Then(/^I should (not )?see an option to login with a Cooper Union account$/) do |negator|
+  expectations_for_page(page, negator, *cu_option_matchers)
 end
 
-Then(/^I should not see an option to login with a Facebook account$/) do
-  expect(page).not_to have_content 'Facebook'
-  expect(page).not_to have_css '.facebook.alt-login'
+Then(/^I should (not )?see an option to login with a NYSID account$/) do |negator|
+  expectations_for_page(page, negator, *nysid_option_matchers)
 end
 
-Then(/^I should not see the NYU torch login button$/) do
-  expect(page).not_to have_content 'Login with an NYU NetID'
-  expect(page).not_to have_css '#shibboleth .btn'
+Then(/^I should (not )?see an option to login with an NYU Libraries Affiliates' account$/) do |negator|
+  expectations_for_page(page, negator, *bobst_option_matchers)
 end
 
-Then(/^I should not see an option to login with a NYSID account$/) do
-  expect(page).not_to have_content 'New York School of Interior Design'
-  expect(page).not_to have_css '.nysid.alt-login'
+Then(/^I should (not )?see an option to login with a Twitter account$/) do |negator|
+  expectations_for_page(page, negator, *twitter_option_matchers)
 end
 
-Then(/^I should be able to login with a Cooper Union account$/) do
-  expect(page).to have_content 'Login with your Cooper Union patron ID'
-  expect(page).to have_content 'Enter your ID Number'
-  expect(page).to have_content 'First four letter of your last name'
+Then(/^I should (not )?see an option to login with a Facebook account$/) do |negator|
+  expectations_for_page(page, negator, *facebook_option_matchers)
 end
 
-Then(/^I should be able to login with a NYSID account$/) do
-  expect(page).to have_content 'Login with your NYSID patron ID'
-  expect(page).to have_content 'Enter your ID Number'
-  expect(page).to have_content 'First four letter of your last name'
+When(/^I press the New School login option$/) do
+  click_link 'New School Libraries'
+end
+
+Then(/^I should go to the New School login page$/) do
+  expect(current_path).to eq(login_path('ns'))
 end
