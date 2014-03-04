3735c9336e81966825237c7d0b368f31e4386c02
Scot Dalton <scotdalton@gmail.com>
2014-03-04 16:22:27 -0500

    Update feature helpers autoloading to namespace the helpers in the LoginFeatures module

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -3,8 +3,8 @@ Coveralls.wear_merged!('rails')
 
 # Require and include helper modules
 # in feature/support/helpers and its subdirectories.
-Dir[Rails.root.join("features/support/helpers/**/*.rb")].each do |f|
-  require f
-  module_constant = f.camelize.demodulize.split('.').first.constantize
-  Cucumber::Rails::World.send(:include, module_constant)
+Dir[Rails.root.join("features/support/helpers/**/*.rb")].each do |helper|
+  require helper
+  helper_name = "LoginFeatures::#{helper.camelize.demodulize.split('.').first}"
+  Cucumber::Rails::World.send(:include, helper_name.constantize)
 end
