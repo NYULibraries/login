0e263e246a1453f96c938132a4715a6656ff7056
Scot Dalton <scotdalton@gmail.com>
2014-03-04 16:02:22 -0500

    Require and include support helper modules in the cucumber local environment

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -1,2 +1,10 @@
 require 'coveralls'
 Coveralls.wear_merged!('rails')
+
+# Require and include helper modules
+# in feature/support/helpers and its subdirectories.
+Dir[Rails.root.join("features/support/helpers/**/*.rb")].each do |f|
+  require f
+  module_constant = f.camelize.demodulize.split('.').first.constantize
+  Cucumber::Rails::World.send(:include, module_constant)
+end
