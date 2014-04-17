e969efe4444408133e61ca702682b8a2fcff3e10
B Alter <barnaby.alter@gmail.com>
2014-04-17 15:07:20 -0400

    Testing jenkins magic branch deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
 set :rails_env, "staging"
-set(:branch, ENV["GIT_BRANCH"].gsub(/remotes\/origin\//,"")) unless exists?(:branch)
+set(:branch, ENV["GIT_BRANCH"].gsub(/remotes\//,"").gsub(/origin\//,"")) #unless exists?(:branch)
