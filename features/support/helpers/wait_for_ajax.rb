d7c0a710496105591005ad902a4105963e233854
hab278 <hab278@nyu.edu>
2014-05-07 12:39:45 -0400

    Wait for ajax working for poltergeist

--- features/support/helpers/wait_for_ajax.rb
+++ features/support/helpers/wait_for_ajax.rb
@@ -6,8 +6,18 @@ module LoginFeatures
       end
     end
 
+    private
+
     def finished_all_ajax_requests?
-      page.evaluate_script('jQuery.active').zero?
+      jquery_loaded && ajax_completed
+    end
+
+    def jquery_loaded
+      page.evaluate_script('typeof $ !== "undefined" && $ !== null')
+    end
+
+    def ajax_completed
+      page.evaluate_script('$.active').zero?
     end
   end
 end
