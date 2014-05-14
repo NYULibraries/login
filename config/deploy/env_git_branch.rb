0b978f6265e6d67766ab84ae14911c943f069b79
hab278 <hab278@nyu.edu>
2014-05-14 12:44:11 -0400

    Document new env_git_branch method

--- config/deploy/env_git_branch.rb
+++ config/deploy/env_git_branch.rb
@@ -1,3 +1,5 @@
+# Gets the Git branch from environment and strips it of superflous information for
+# capistrano. Raises an error if GIT_BRANCH was not set and explains.
 def env_git_branch
   ENV["GIT_BRANCH"].gsub(/remotes\//,"").gsub(/origin\//,"")
 rescue NoMethodError => msg
