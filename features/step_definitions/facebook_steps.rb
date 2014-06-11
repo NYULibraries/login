1cd01a3829e1b6fcf1d51f46d89dcee065dff280
hab278 <hab278@nyu.edu>
2014-06-11 12:29:52 -0400

    Identify steps for facebook login feature.

--- features/step_definitions/facebook_steps.rb
+++ features/step_definitions/facebook_steps.rb
@@ -1,18 +1,12 @@
 When(/^Facebook authenticates me$/) do
   # if VCR.current_cassette.recording?
-    # expectations_for_page(page, nil, *facebook_style_matchers)
-    # within("#oauth_form") do
-      # fill_in 'Username or email', with: ""#username_for_location("Facebook")
-      # fill_in 'Password', with: "testpass"#password_for_location("Facebook")
-      # click_button 'Sign In'
-    # end
+    expectations_for_page(page, nil, *facebook_style_matchers)
+    fill_in("email", with: username_for_location("Facebook"))
+    fill_in("pass", with: password_for_location("Facebook"))
+    click_button "Log In"
   # else
   #   visit '/users/auth/twitter/callback?institute=NYU'
   # end
-  # fill_in("email", with: "sharon_dwzwdap_bharambeberg@tfbnw.net")
-  # fill_in("pass", with: "testpass")
-  # click_button "Log In"
-  # binding.pry
 end
 
 When(/^I've authorized Facebook to share my information with NYU Libraries$/) do
@@ -24,5 +18,5 @@ When(/^I wait up to (\d+) seconds for Facebook to redirect me$/) do |timeout|
 end
 
 Then(/^I should be logged in with my Facebook handle$/) do
-  # expectations_for_page(page, nil, *logged_in_matchers("Facebook"))
+  expectations_for_page(page, nil, *logged_in_matchers("Facebook"))
 end
