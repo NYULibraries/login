36da41acab6404598910a2c620fa191864d1354b
hab278 <hab278@nyu.edu>
2014-06-18 16:40:25 -0400

    clean up steps

--- features/step_definitions/facebook_steps.rb
+++ features/step_definitions/facebook_steps.rb
@@ -1,21 +1,8 @@
 When(/^Facebook authenticates me$/) do
-  # if VCR.current_cassette.recording?
     fill_in("email", with: username_for_location("Facebook"))
     fill_in("pass", with: password_for_location("Facebook"))
     click_button "Log In"
-  #   binding.pry
-  # else
-  #   binding.pry
-  #   # visit '/users/auth/facebook/callback'
-  # end
-end
 
-When(/^I've authorized Facebook to share my information with NYU Libraries$/) do
- # Do nothing
-end
-
-When(/^I wait up to (\d+) seconds for Facebook to redirect me$/) do |timeout|
-  # wait_for_ajax timeout.to_i
 end
 
 Then(/^I should be logged in with my Facebook handle$/) do
