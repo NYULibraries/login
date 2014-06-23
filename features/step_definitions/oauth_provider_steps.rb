4d4e7098de143adf7b453bd46f615936a4cf5555
hab278 <hab278@nyu.edu>
2014-06-23 14:49:01 -0400

    Change tests based on previous commits.

--- features/step_definitions/oauth_provider_steps.rb
+++ features/step_definitions/oauth_provider_steps.rb
@@ -1,27 +1,22 @@
-Before('@client_app') do
-  visit login_path
-  url = URI.parse(current_url)
-  @site = "#{url.scheme}://#{url.host}:#{url.port}"
-end
-
 Given(/^I am on an NYU client application$/) do
+  set_provider_url
   expect(client).not_to be_nil
   expect(client).to be_instance_of OAuth2::Client
 end
 
-When(/^I login via NYU Shibboleth$/) do
-  # Log user in via NYU Shibboleth
-  login_as_nyu_shibboleth
-  # Visit the callback to ensure login and user creation
-  visit nyu_home_url
-  # Make sure this user is authorized for this app
-  oauth_app.authorized_tokens.create(resource_owner_id: current_resource_owner.id)
+When(/^I have (previously )?logged in to Login as an NYU Shibboleth user$/) do |previously|
+  login_and_authorize_nyu_shibboleth_user
 end
 
-Then(/^NYU Libraries' Login authorizes me for the client application$/) do
+Then(/^I should see the Login page$/) do
+  expectations_for_page(page, "", *nyu_login_matchers)
+  # pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I click Login on the client application$/) do
   # Visit the auth url for this client
-  visit auth_url
-  expect(auth_code).to have_content
+  visit client_authorize_url
+  expect(auth_code).to have_content if current_resource_owner
 end
 
 Then(/^I should be logged in to the NYU client application$/) do
