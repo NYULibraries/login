2fbb25d97c3a141e71f19869c94ac78874b811ed
hab278 <hab278@nyu.edu>
2014-06-24 17:59:41 -0400

    Create factories for new providers.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -9,6 +9,51 @@ FactoryGirl.define do
     admin false
   end
 
+  factory :nyu_shibboleth_user, class: User do
+    username 'developer'
+    email 'developer@example.com'
+    institution_code 'NYU'
+    provider "nyu_shibboleth"
+    current_sign_in_at Time.now
+    admin false
+  end
+
+  factory :new_school_ldap_user, class: User do
+    username 'developer'
+    email 'developer@example.com'
+    institution_code 'NYU'
+    provider "new_school_ldap"
+    current_sign_in_at Time.now
+    admin false
+  end
+
+  factory :facebook_user, class: User do
+    username 'developer'
+    email 'developer@example.com'
+    institution_code 'NYU'
+    provider "facebook"
+    current_sign_in_at Time.now
+    admin false
+  end
+
+  factory :twitter_user, class: User do
+    username 'developer'
+    email 'developer@example.com'
+    institution_code 'NYU'
+    provider "twitter"
+    current_sign_in_at Time.now
+    admin false
+  end
+
+  factory :aleph_user, class: User do
+    username 'developer'
+    email 'developer@example.com'
+    institution_code 'NYU'
+    provider "aleph"
+    current_sign_in_at Time.now
+    admin false
+  end
+
   factory :admin, class: User do
     username 'admin'
     email 'admin@example.com'
