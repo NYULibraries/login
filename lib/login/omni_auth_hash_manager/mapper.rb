3db0fdd85ebbe692da69f34ddf77991e763d6b60
B Alter <barnaby.alter@gmail.com>
2014-06-25 16:20:14 -0400

    Commented up Mapper and Validator

--- lib/login/omni_auth_hash_manager/mapper.rb
+++ lib/login/omni_auth_hash_manager/mapper.rb
@@ -1,33 +1,53 @@
 ##
+# Class to map an omniauth hash returned by a response in the environment
+# into an object and make attribute decisions based on provider
 module Login
   module OmniAuthHashManager
     class Mapper
 
+      ##
+      # Create a new mapper object from the omniauth hash
+      # and delegate the validation to the validator class
+      #
+      # Example:
+      #   Mapper.new(OmniAuth::AuthHash)
       def initialize(omniauth_hash)
         validator = Login::OmniAuthHashManager::Validator.new(omniauth_hash)
         @omniauth_hash_mapper = omniauth_hash
       end
 
+      ##
+      # Return the uid attribute
       def uid
         @omniauth_hash_mapper.uid
       end
 
+      ##
+      # Return the provider attribute
       def provider
         @omniauth_hash_mapper.provider
       end
 
+      ##
+      # Return the info attribute, which is an OmniAuth::AuthHash::InfoHash
       def info
         @omniauth_hash_mapper.info
       end
 
+      ##
+      # Return the email attribute form the InfoHash
       def email
         @omniauth_hash_mapper.info.email
       end
 
+      ##
+      # Generate the properties hash from InfoHash and extra fields
       def properties
         @omniauth_hash_mapper.info.merge(extra: @omniauth_hash_mapper.extra)
       end
 
+      ##
+      # Return username attr based on provider
       def username
         username = begin
           case @omniauth_hash_mapper.provider
@@ -44,6 +64,8 @@ module Login
         username.downcase unless username.nil?
       end
 
+      ##
+      # Return OmniAuth::AuthHash representation
       def to_hash
         @omniauth_hash_mapper
       end
--- lib/login/omni_auth_hash_manager/validator.rb
+++ lib/login/omni_auth_hash_manager/validator.rb
@@ -1,28 +1,44 @@
 ##
+# Class to validate an OmniAuth hash for different conditions
+# and raise an ArgumentError if it's not
 module Login
   module OmniAuthHashManager
     class Validator
 
+      # Call this error if omniauth hash is invalid
+      # handle it as a global catch in the application controller
       class ArgumentError < ::ArgumentError
         def initialize(omniauth_hash)
           super("#{omniauth_hash} is not a valid OmniAuth::AuthHash")
         end
       end
 
+      ##
+      # Create a new validator object with an OmniAuth::AuthHash and an optional provider
+      # and raise and ArgumentError if the hash is invalid
+      #
+      # Example:
+      #   Validator.new(OmniAuth::AuthHash, 'nyu_shibboleth|newschool_ldap|twitter|facebook|etc')
       def initialize(omniauth_hash, provider = nil)
         @omniauth_hash = omniauth_hash
         @provider = provider if provider
         raise ArgumentError.new(omniauth_hash) unless self.valid?
       end
 
+      ##
+      # Return boolean if hash is valid
       def valid?
         (@provider) ? self.valid_for_action? : self.valid_hash?
       end
 
+      ##
+      # If provider is present make sure it matches the provider from the hash
       def valid_for_action?
         self.valid_hash? && @omniauth_hash.provider == @provider
       end
 
+      ##
+      # Validate the hash's presence and type
       def valid_hash?
         @omniauth_hash.present? && @omniauth_hash.is_a?(OmniAuth::AuthHash)
       end
