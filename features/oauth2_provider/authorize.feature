d1114a5d5581a6e2269b85991c34e1533f963a66
B Alter <barnaby.alter@gmail.com>
2014-06-27 16:39:36 -0400

    Reworked provider features to be three separate features with two scenarios a piece

---
+++ features/oauth2_provider/authorize.feature
@@ -0,0 +1,18 @@
+@omniauth_test
+Feature: Authorize an OAuth2 client application
+  In order to allow an OAuth2 client application to have access to exposed user attributes
+  As a user of the OAuth2 client application
+  I want to automatically authorize an OAuth2 client application to get user attributes from Login
+
+  Scenario: A logged out user can login and be authorized
+    Given I am logged out
+    And I am on an OAuth2 client application
+    When I login to Login as an NYU Shibboleth user
+    Then I should be automatically authorized to use Login as my provider
+    And the OAuth2 client should have access to exposed attributes
+
+  Scenario: A logged in user is authorized
+    Given I have previously logged in to Login as an NYU Shibboleth user
+    And I am on an OAuth2 client application
+    Then I should be automatically authorized to use Login as my provider
+    And the OAuth2 client should have access to exposed attributes
--- features/step_definitions/oauth_provider_steps.rb
+++ features/step_definitions/oauth_provider_steps.rb
@@ -2,16 +2,15 @@ Given(/^I am logged out$/) do
   OmniAuth.config.mock_auth[:nyu_shibboleth] = nil
 end
 
-Given(/^I am on an NYU client application$/) do
+Given(/^I am on an OAuth2 client application$/) do
   expect(client).not_to be_nil
   expect(client).to be_instance_of OAuth2::Client
 end
 
-Then(/^I click "Login" on the client application$/) do
+When(/^I click "Login"$/) do
   # Visit the auth url for this client
   visit client_authorize_url
   click_on "Click to Login"
-  expect(auth_code).to have_content if current_resource_owner
 end
 
 When(/^I have previously logged in to Login as an NYU Shibboleth user$/) do
@@ -25,11 +24,15 @@ When(/^I login to Login as an NYU Shibboleth user$/) do
   click_on "Click to Login"
 end
 
-Then(/^I should see the Login page$/) do
-  expectations_for_page(page, "", *nyu_login_matchers)
+Then(/^I should (not )?see the Login page$/) do |negator|
+  expectations_for_page(page, negator, *nyu_login_matchers)
 end
 
-Then(/^I should be logged in to the NYU client application$/) do
+Then(/^I should be logged in to the OAuth2 client application$/) do
+  expect(authorization_code).to have_content
+end
+
+Then(/^the OAuth2 client should have access to exposed attributes$/) do
   VCR.use_cassette("get access token", match_requests_on: [:path], record: :all) do
     get api_v1_user_path(:access_token => access_token)
     expect(last_response.body).to include "snowj@newschool.edu"
