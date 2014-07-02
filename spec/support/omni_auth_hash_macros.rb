79e80bf736a4716cfe54085b0d4ae197216df866
B Alter <barnaby.alter@gmail.com>
2014-07-02 15:17:29 -0400

    Added an OmniAuth::AuthHash factory and changed macros

--- spec/support/omni_auth_hash_macros.rb
+++ spec/support/omni_auth_hash_macros.rb
@@ -1,18 +1,10 @@
 module OmniAuthHashMacros
   def authhash(provider)
-    authhash = OmniAuth::AuthHash.new
-    attributes = (FactoryGirl.factories.registered?("#{provider}_identity")) ? attributes_for("#{provider}_identity") : attributes_for(:invalid_identity, provider: provider)
-    authhash.uid = attributes[:uid]
-    authhash.provider = "#{provider}"
-    properties = attributes[:properties]
-    name = properties[:name]
-    email = properties[:email]
-    nickname = properties[:nickname]
-    phone = properties[:phone]
-    authhash.info =
-      infohash({ name: name, email: email, nickname: nickname, phone: phone })
-    authhash.extra = attributes[:properties][:extra]
-    authhash
+    if FactoryGirl.factories.registered?("#{provider}_authhash")
+      create("#{provider}_authhash")
+    else
+      create(:invalid_provider_authhash, provider: provider)
+    end
   end
 
   def authhash_map(provider)
