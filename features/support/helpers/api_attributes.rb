377033590de72323567a9488320a0b6954f9612d
hab278 <hab278@nyu.edu>
2014-07-10 15:12:00 -0400

    Add field translations for aleph.

--- features/support/helpers/api_attributes.rb
+++ features/support/helpers/api_attributes.rb
@@ -7,6 +7,11 @@ module LoginFeatures
     def field_titles
       {
         "NetID" => "uid",
+        "Aleph ID" => "uid",
+        "Patron Status" => "z305_bor_status",
+        "Patron Type" => "z305_bor_type",
+        "ILL Permission" => "z305_photo_permission",
+        "PLIF Status" => "z303_birthplace",
         "Given Name" => "first_name",
         "Surname" => "last_name",
         "Entitlement" => "entitlement",
