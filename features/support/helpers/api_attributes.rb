624d4f119d0cf0b3d741c9ca3a876a8b3b0994f0
hab278 <hab278@nyu.edu>
2014-07-23 15:30:57 -0400

    Use parameterize when available.

--- features/support/helpers/api_attributes.rb
+++ features/support/helpers/api_attributes.rb
@@ -1,17 +1,13 @@
 module LoginFeatures
   module ApiAttributes
     def map_title_to_field(title)
-      field_titles[title]
+       field_titles[title] or title.parameterize.underscore
     end
 
     def field_titles
       {
         "NetID" => "uid",
         "Aleph ID" => "uid",
-        "Patron Status" => "z305_bor_status",
-        "Patron Type" => "z305_bor_type",
-        "ILL Permission" => "z305_photo_permission",
-        "PLIF Status" => "z303_birthplace",
         "Given Name" => "first_name",
         "Surname" => "last_name",
         "Entitlement" => "entitlement",
