882378836ca94db7dfc04bea0b15921658b05aee
hab278 <hab278@nyu.edu>
2014-07-28 13:06:56 -0400

    Reorder and redo step definitions.

--- features/step_definitions/authorize_apps_steps.rb
+++ features/step_definitions/authorize_apps_steps.rb
@@ -1,32 +1,42 @@
-Given(/^I am logged in as a "(.*?)" admin$/) do |location|
-  set_admin_login_env_for_location location
+Given(/^I am logged in as an admin$/) do
+  set_admin_login_env
   visit client_authorize_url
-  follow_login_steps_for_location location
+  login_as_admin
+  visit "/login"
+end
+
+Given(/^I am a logged out user$/) do
+  ensure_logout
+  visit "/login"
 end
 
 When(/^I go to the client applications page$/) do
   visit "/oauth/applications"
 end
 
-Then(/^I should (not )?have access to the list of applications$/) do |negator|
-  expectations_for_page(page, negator, *authorize_applications_matchers)
+When(/^I go to the new client applications page$/) do
+  visit "/oauth/applications/new"
+end
+
+When(/^I click on the applications link$/) do
+  click_on "applications"
+end
+
+When(/^I fill out the form with the following fields:$/) do |table|
+  table.rows_hash.each do |field_title, value|
+    fill_in field_title, with: value
+  end
+  click_button "Submit"
 end
 
 When(/^I click add new application$/) do
   click_link "New Application"
 end
 
-When(/^I add the application$/) do
-  fill_in "application_name", with: "test"
-  fill_in "application_redirect_uri", with: "urn:ietf:wg:oauth:2.0:oob"
-  click_button "Submit"
+Then(/^I should (not )?have access to the list of applications$/) do |negator|
+  expectations_for_page(page, negator, *authorize_applications_matchers)
 end
 
 Then(/^I should see that the application is added$/) do
   expect(page).to have_text("Application created.")
-end
-
-Given(/^I am a logged out user$/) do
-  OmniAuth.config.mock_auth.clear
-  visit "/login"
 end
