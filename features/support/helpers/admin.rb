3386e0f49d004c80a2ccae5c1554ecf4d3d82b40
hab278 <hab278@nyu.edu>
2014-07-28 13:17:57 -0400

    Make LoginFeatures::Admin much more manageable and independent of provider.

--- features/support/helpers/admin.rb
+++ features/support/helpers/admin.rb
@@ -1,45 +1,26 @@
 module LoginFeatures
   module Admin
-    def set_admin_account(provider)
-      FactoryGirl.create(
-        :admin,
-        provider: provider.to_s,
-        omniauth_hash_map: Login::OmniAuthHash::Mapper.new(admin_omniauth_hash(provider)),
-        username: admin_omniauth_hash(provider).uid
-      )
+    Provider = FactoryGirl.attributes_for(:admin)[:provider]
+    def set_admin_login_env
+      set_admin_account
+      OmniAuth.config.mock_auth[Provider.to_sym] = admin_omniauth_hash
     end
 
-    def set_admin_login_env(provider)
-      OmniAuth.config.mock_auth[provider.to_sym] = admin_omniauth_hash(provider)
+    def omniauth_hash_map
+      @omniauth_hash_map ||= Login::OmniAuthHash::Mapper.new(admin_omniauth_hash)
     end
 
-    def admin_omniauth_hash(provider)
-      @admin_omniauth_hash ||= OmniAuth::AuthHash.new(FactoryGirl.create("#{provider}_authhash".to_sym))
+    def admin_omniauth_hash
+      @admin_omniauth_hash ||= OmniAuth::AuthHash.new(FactoryGirl.create("#{Provider}_authhash".to_sym))
     end
 
-    def set_admin_login_env_for_location(location)
-      case location
-      when /New School LDAP$/
-        set_admin_account(:new_school_ldap)
-        set_admin_login_env(:new_school_ldap)
-      when /NYU Shibboleth$/
-        set_admin_account(:nyu_shibboleth)
-        set_admin_login_env(:nyu_shibboleth)
-      when /Aleph$/
-        set_admin_account(:aleph)
-        set_admin_login_env(:aleph)
-      else
-        raise "Unknown location!"
-      end
+    def login_as_admin
+      follow_login_steps_for_location provider_to_location[Provider]
     end
 
-    # def set_invalid_admin_login_env
-    #   OmniAuth.config.mock_auth[:new_school_ldap] = :invalid_credentials
-    # end
-    #
-    # def new_school_callback_url
-    #   user_omniauth_authorize_path(:provider => "new_school_ldap")
-    # end
-
+    def set_admin_account
+      FactoryGirl.create(:admin, omniauth_hash_map: omniauth_hash_map, username: omniauth_hash_map.username)
+    end
+    private :set_admin_account
   end
 end
