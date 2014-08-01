2681f53b0b7dd9c9d974d5fd427fad043a4c2c4b
hab278 <hab278@nyu.edu>
2014-08-01 15:09:20 -0400

    Add more step definitions.

--- features/step_definitions/authorize_apps_steps.rb
+++ features/step_definitions/authorize_apps_steps.rb
@@ -10,6 +10,14 @@ Given(/^I am a logged out user$/) do
   visit "/login"
 end
 
+Given(/^I am logged in as a non\-admin user$/) do
+  step "I am logged in as a \"New School LDAP\" user"
+end
+
+When(/^I go to my user page$/) do
+  visit "/login"
+end
+
 When(/^I go to the client applications page$/) do
   visit "/oauth/applications"
 end
@@ -33,10 +41,18 @@ When(/^I click add new application$/) do
   click_link "New Application"
 end
 
-Then(/^I should (not )?have access to the list of applications$/) do |negator|
+Then(/^I should (not )?see the list of applications$/) do |negator|
   expectations_for_page(page, negator, *authorize_applications_matchers)
 end
 
 Then(/^I should see that the application is added$/) do
   expect(page).to have_text("Application created.")
+end
+
+Then(/^I should not see the form to add a new application$/) do
+  expect(page).to_not have_css("#new_application")
+end
+
+Then(/^I should not see the link to the list of client applications$/) do
+  expect(page).to_not have_link("applications")
 end
