27f6e486ea576409f57fe1dfb80460c0879bb494
B Alter <barnaby.alter@gmail.com>
2014-08-08 18:18:39 -0400

    Manually changed VHS tape

--- spec/login/aleph/patron_loader/bor_info_strategy_spec.rb
+++ spec/login/aleph/patron_loader/bor_info_strategy_spec.rb
@@ -12,11 +12,17 @@ module Login
       end
       describe '#patron' do
         subject { bor_info_strategy.patron }
-        its(:identifier) { should eql "BOR_ID" }
-        its(:plif_status) { should eql "PLIF LOADED" }
-        its(:status) { should eql "NYU Administrator" }
-        its(:type) { should eql "CB" }
-        its(:ill_permission) { should eql "Y" }
+        context "when identifier is valid and returns a BorInfo object" do
+          its(:identifier) { should eql "BOR_ID" }
+          its(:plif_status) { should eql "PLIF LOADED" }
+          its(:status) { should eql "NYU Administrator" }
+          its(:type) { should eql "CB" }
+          its(:ill_permission) { should eql "Y" }
+        end
+        context "when identifier is invalid" do
+          let(:identifier) { 'INVALID_ID' }
+          it { should be_nil }
+        end
       end
     end
   end
