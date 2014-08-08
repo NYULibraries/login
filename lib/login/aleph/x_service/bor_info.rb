bab7e981417f565ad43b402442756d46403370d0
B Alter <barnaby.alter@gmail.com>
2014-08-08 18:38:03 -0400

    Moved bor-info specific options to BorInfo class

--- lib/login/aleph/x_service/bor_info.rb
+++ lib/login/aleph/x_service/bor_info.rb
@@ -4,6 +4,9 @@ module Login
       class BorInfo < Base
         attr_accessor :identifier, :library
 
+        DEFAULT_SHOW_LOANS_VALUE = "N"
+        DEFAULT_SHOW_CASH_VALUE = "N"
+
         def initialize(identifier, library = ENV["ALEPH_XSERVICE_LIBRARY"])
           @identifier = identifier
           @library = library
@@ -16,6 +19,13 @@ module Login
 
         def error?
           error.present?
+        end
+
+        def options
+          @options ||= super.merge({
+            loans: DEFAULT_SHOW_LOANS_VALUE,
+            cash: DEFAULT_SHOW_CASH_VALUE
+          })
         end
 
       protected
