0b3a8d6866d20b5de27cb37a5a0e6d5fe762ddbe
B Alter <barnaby.alter@gmail.com>
2014-08-08 18:42:06 -0400

    Started TDD for BorInfo

--- spec/login/aleph/x_service/bor_info_spec.rb
+++ spec/login/aleph/x_service/bor_info_spec.rb
@@ -3,7 +3,13 @@ module Login
   module Aleph
     module XService
       describe BorInfo do
-        
+        let(:identifier) { 'BOR_ID' }
+        subject(:bor_info) { Aleph::XService::BorInfo.new(identifier) }
+        it { should be_a Aleph::XService::BorInfo }
+        describe '#identifier' do
+          subject { bor_info.identifier }
+          it { should eq identifier }
+        end
       end
     end
   end
