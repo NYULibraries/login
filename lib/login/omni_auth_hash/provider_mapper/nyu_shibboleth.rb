c9770c7ea3d098f88b1049573ce2b08456fbc3f4
B Alter <barnaby.alter@gmail.com>
2014-08-13 17:32:32 -0400

    Changed method of merging extra attributes to move entitlement to top level of extra

--- lib/login/omni_auth_hash/provider_mapper/nyu_shibboleth.rb
+++ lib/login/omni_auth_hash/provider_mapper/nyu_shibboleth.rb
@@ -7,7 +7,16 @@ module Login
       class NYUShibboleth < Base
         def initialize(omniauth_hash)
           super(omniauth_hash)
-          @nyuidn = @omniauth_hash.extra.nyuidn
+          @nyuidn = omniauth_hash.extra.raw_info.nyuidn
+          @properties = omniauth_hash.info.merge(extra_attributes)
+        end
+
+        def extra_attributes
+          super().merge({
+            extra: super()[:extra].merge({
+              entitlement: omniauth_hash.extra.raw_info.entitlement
+            })
+          })
         end
       end
     end
