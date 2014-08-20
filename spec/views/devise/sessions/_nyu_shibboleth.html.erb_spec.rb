c30ed5fc55e46f48ce36bc8a5e411401ed8f7ae1
B Alter <barnaby.alter@gmail.com>
2014-08-20 16:05:22 -0400

    Updated view specs to work with new design

--- spec/views/devise/sessions/_nyu_shibboleth.html.erb_spec.rb
+++ spec/views/devise/sessions/_nyu_shibboleth.html.erb_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "devise/sessions/_nyu_shibboleth.html.erb" do
   subject { render; rendered }
   it do
-    should match(/<div id="shibboleth">/)
-    should match(/<h2>\s+Login with an NYU NetID/)
+    should match(/<div id="nyu_shibboleth-login"/)
+    should match(/Select your affiliation to begin/)
     should match(/href="\/users\/auth\/nyu_shibboleth\?institute=NYU"/)
-    should match('Click to Login')
+    should match('NYU')
     should_not match("<a class=\"nyulibraries-help nyulibraries-help-icon\" ")
   end
 end
