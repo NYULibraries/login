1c459b0011c4d367ae496dd05de3da6d1844d7f9
hab278 <hab278@nyu.edu>
2014-09-30 14:43:42 -0400

    Add encoding option to flatfile object

--- lib/login/aleph/flat_file.rb
+++ lib/login/aleph/flat_file.rb
@@ -2,16 +2,17 @@ module Login
   module Aleph
     class FlatFile
       attr_reader :location
-      def initialize(location)
+      def initialize(location, encoding="UTF-8")
         raise ArgumentError, 'location cannot be nil' if location.nil?
         raise ArgumentError, 'File not found!' unless File.exist?(location)
         @location = location
+        @encoding = encoding
       end
 
       def find_patron_by_identifier(identifier)
         raise Errno::ENOENT unless location && File.exist?(location)
         flat_file_line = nil
-        File.open(location,'r') do |file|
+        File.open(location,"r:#{@encoding}") do |file|
           while line = file.gets
             flat_file_line = FlatFileLine.new(line)
             break if flat_file_line.identifier.eql?(identifier)
