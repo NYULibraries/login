21b3a92219199bc247d11f0d3f3ab263af0f76f9
B Alter <barnaby.alter@gmail.com>
2015-02-06 14:46:06 -0500

    Added some comments

--- lib/login/aleph/flat_file/flat_file_line.rb
+++ lib/login/aleph/flat_file/flat_file_line.rb
@@ -2,6 +2,7 @@ module Login
   module Aleph
     class FlatFile
       class FlatFileLine
+        # An ordered list of the tab-delimited values found in the flat file
         ATTRIBUTES = [:identifier, :barcode, :verification, :expiry_date,
           :patron_status, :patron_type, :bor_name, :email, :ill_permission, :plif_status,
           :college_code, :college, :dept_code, :department, :major_code,
@@ -17,9 +18,13 @@ module Login
           end
         end
 
+        # Turn a line in the flat file into a patron object
+        # Note that this sets all the above attributes on the new object
+        # only if Patron has an attr_accessor for that attribute
         def to_patron
           Patron.new do |patron|
             ATTRIBUTES.each do |attribute|
+              # => if patron.respond_to?("expiry_date=") == false => patron.expiry_date == nil
               patron.send("#{attribute}=", self.send("#{attribute}")) if patron.respond_to?("#{attribute}=")
             end
           end
