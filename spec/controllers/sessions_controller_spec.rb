c4fda9af7f5c7b03285c97243ab66422419fc220
B Alter <barnaby.alter@gmail.com>
2015-05-13 14:39:24 -0400

    Wrote specs nyu shibboleth as well and found a typo

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,9 +10,16 @@ describe SessionsController do
       subject { get :destroy }
       it { should be_redirect }
       it("should have a 302 status") { expect(subject.status).to be(302) }
-      it { should redirect_to(logged_out_url(:nyu)) }
       it("should delete the login_sso cookie") do
         expect(subject.cookies['_login_sso']).to be_nil
+      end
+      context 'when logged in as an nyu shibboleth user' do
+        let(:provider) { 'nyu_shibboleth' }
+        it { should redirect_to('https://login.nyu.edu/sso/UI/Logout') }
+      end
+      context 'when logged in as a new school user' do
+        let(:provider) { 'new_school_ldap' }
+        it { should redirect_to(logged_out_url(:nyu)) }
       end
     end
   end
