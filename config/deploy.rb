cfe84c1e2e70a946e9b551fb8d1d8c2eaa9570a9
B Alter <barnaby.alter@gmail.com>
2015-07-20 16:43:45 -0400

    Updated web services email

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'formaggio/capistrano'
-set :recipient, "web.services@library.nyu.edu"
+set :recipient, "lib-webservices@nyu.edu"
 set :app_title, "login"
 set :keep_releases, 5
 set :rvm_ruby_string, "2.1.3"
