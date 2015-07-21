913bf6d7fc0ac0a28d7886458e1aef9c73719bf1
B Alter <barnaby.alter@gmail.com>
2015-07-21 12:21:13 -0400

    Force session destroy when GET to /logged_out so there is no confusion about when user is logged out

--- app/controllers/wayf_controller.rb
+++ app/controllers/wayf_controller.rb
@@ -1,4 +1,12 @@
 class WayfController < ApplicationController
   respond_to :html
+  # Don't allow users to visit the logged_out page with GET, force a logout if that happens
+  before_filter :redirect_to_logout, only: [:logged_out], if: -> { user_signed_in? }
+
+ private
+
+  def redirect_to_logout
+    redirect_to logout_path
+  end
 
 end
