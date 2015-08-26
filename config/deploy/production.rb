b82a3183df7975cc407195014395e4373d758e20
B Alter <barnaby.alter@gmail.com>
2015-08-26 18:19:44 -0400

    Trying another chain of cap tasks for loading the schema

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@ set :branch, "master"
 
 namespace :deploy do
   task :schema_load do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:schema:load"
+    run "cd #{latest_release}; RAILS_ENV=#{rails_env} bundle exec rake db:schema:load"
   end
 end
 
-before "deploy:migrate", "deploy:schema_load"
+before "deploy:migrations", "deploy:schema_load"
