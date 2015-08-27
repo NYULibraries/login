78696fb8eebfc6fafa21c557280c48c61c4b840d
B Alter <barnaby.alter@gmail.com>
2015-08-27 13:56:06 -0400

    Removed load schema from capistrano chain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,2 @@
 set :rails_env, "production"
 set :branch, "master"
-
-namespace :deploy do
-  task :schema_load do
-    run "cd #{latest_release}; RAILS_ENV=#{rails_env} bundle exec rake db:schema:load"
-  end
-end
-
-before "deploy:migrations", "deploy:schema_load"
