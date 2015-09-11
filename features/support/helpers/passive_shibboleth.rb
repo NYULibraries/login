3926a4b01012c1d0da2891827844685186b9c876
B Alter <barnaby.alter@gmail.com>
2015-09-11 15:36:26 -0400

    Removed passive tests in cukes

--- features/support/helpers/passive_shibboleth.rb
+++ features/support/helpers/passive_shibboleth.rb
@@ -4,7 +4,7 @@ module LoginFeatures
     # This keeps the integration and function of the app the same but forces
     # to keep all redirects local. Win win.
     def ignore_passive_shibboleth
-      UsersController.any_instance.should_receive(:passive_shibboleth_url).and_return("/")
+      #UsersController.any_instance.should_receive(:passive_shibboleth_url).and_return("/")
     end
   end
 end
