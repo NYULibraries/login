2c614ba21e38c90f45f702965ad5a5c212f0a234
B Alter <barnaby.alter@gmail.com>
2015-09-14 19:05:33 -0400

    Added functional tests for passive login functions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,8 +4,10 @@ describe UsersController do
   let(:attributes) { attributes_for(:user) }
   describe "GET 'show'" do
     render_views false
-    context 'when not logged in' do
-    
+    context 'when logged out' do
+      before { get :show }
+      subject { response }
+      it { should redirect_to login_path }
     end
     context 'when logged in' do
       login_user
@@ -226,6 +228,79 @@ describe UsersController do
         it { should be_redirect }
         it { should redirect_to(auth_url(auth_type, 'nyu')) }
       end
+    end
+  end
+
+  describe 'GET /login/passive' do
+    render_views false
+    let(:client_id) { create(:oauth_application, redirect_uri: 'https://trustedapp.nyu.edu').uid }
+    let(:return_uri) { 'https://trustedapp.nyu.edu/i/was/in/the/middle/of/something' }
+    let(:login_path) { nil }
+    let(:params) { { client_id: client_id, return_uri: return_uri, login_path: login_path } }
+    let(:session_params) { nil }
+    context 'when user is logged out' do
+      before { get :client_passive_login, params, session_params }
+      subject { response }
+      context 'and hasnt made a call to Shibboleth Idp yet' do
+        let(:escaped_return_uri) { "#{CGI::escape(return_uri)}" }
+        let(:escaped_origin) { "#{CGI::escape("http://test.host/login/passive?client_id=#{client_id}&return_uri=#{escaped_return_uri}")}" }
+        let(:target_url) { CGI::escape("http://test.host/login/passive_shibboleth?origin=#{escaped_origin}") }
+        it { should redirect_to "http://test.host/Shibboleth.sso/Login?isPassive=true&target=#{target_url}" }
+      end
+      context 'and has already made a call to Shibboleth Idp' do
+        let(:session_params) { {:_check_passive_shibboleth => true} }
+        context 'and the return uri is whitelisted' do
+          it { should redirect_to 'https://trustedapp.nyu.edu/i/was/in/the/middle/of/something' }
+        end
+        context 'but the return uri is not whitelisted or mismatches the client return url' do
+          let(:client_id) { create(:oauth_application) }
+          let(:return_uri) { 'madeuphost.bg' }
+          its(:status) { should be 400 }
+        end
+      end
+    end
+    context 'when user is logged in' do
+      login_user
+      let(:origin) { CGI::escape('https://trustedapp.nyu.edu/i/was/in/the/middle/of/something') }
+      before { get :client_passive_login, params, session_params }
+      subject { response }
+      context 'and a custom login path is set' do
+        let(:login_path) { '/login' }
+        it { should redirect_to "https://trustedapp.nyu.edu/login?origin=#{origin}" }
+      end
+      context 'and no login path is set' do
+        it { should redirect_to "https://trustedapp.nyu.edu/users/auth/nyulibraries?origin=#{origin}" }
+      end
+    end
+  end
+
+  describe 'GET /login/passive_shibboleth' do
+    let(:origin) { 'https://trustedapp.nyu.edu' }
+    subject { response }
+    context 'when user is logged in to shibboleth' do
+      before { get :shibboleth_passive_login, origin: origin }
+      prepend_before { @request.cookies[:_shibsession_] = 'test123' }
+      it { should redirect_to 'http://test.host/users/auth/nyu_shibboleth?auth_type=nyu&institute=NYU&origin=https%3A%2F%2Ftrustedapp.nyu.edu' }
+    end
+    context 'when user is not logged in to shibboleth' do
+      before { get :shibboleth_passive_login, origin: origin }
+      it { should redirect_to 'https://trustedapp.nyu.edu' }
+    end
+  end
+
+  describe 'GET /passthru' do
+    let(:params) { nil }
+    let(:redirect_uri) { 'https://trustedapp.nyu.edu' }
+    let(:session_params) { { _action_before_eshelf_redirect: redirect_uri } }
+    before { get :passthru, params, session_params }
+    prepend_before { @request.cookies[:_nyulibraries_eshelf_passthru] = { value: 1, httponly: true, domain: '.localhost' } }
+    subject { response }
+    context 'when user has a saved action in session' do
+      it { should redirect_to 'https://trustedapp.nyu.edu' }
+    end
+    context 'when user has no saved action' do
+      let(:redirect_uri) { nil }
+      it { should redirect_to root_path }
     end
   end
 end
