ccb298db093c787bc06ccaa3f20f584fc59e0f22
B Alter <barnaby.alter@gmail.com>
2015-09-14 14:40:26 -0400

    Redirect to eshelf after login and passthru to original action afterwards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,21 +66,25 @@ class UsersController < Devise::OmniauthCallbacksController
   # Redirect to original stored location after being sent back to the Login app
   # from the eshelf login
   def passthru
-    return_uri = session[:_return_uri]
-    session[:_return_uri] = nil
+    # Assuming we authenticated in E-shelf
+    # Return to the original location, on active login
+    # or back to the passive login action, on passive
+    action_before_eshelf_redirect = session[:_action_before_eshelf_redirect]
+    session[:_action_before_eshelf_redirect] = nil
     cookies.delete(ESHELF_COOKIE_NAME, domain: ENV['LOGIN_COOKIE_DOMAIN'])
-    redirect_to stored_location_for("user") || return_uri || signed_in_root_path('user')
+    redirect_to stored_location_for("user") || action_before_eshelf_redirect || signed_in_root_path('user')
   end
 
   def after_sign_in_path_for(resource)
     # If there is an eshelf login variable set then we want to redirect there after login
     # to permanently save eshelf records
-    # if ENV['ESHELF_LOGIN_URL'] && !cookies[ESHELF_COOKIE_NAME]
-    #   create_eshelf_cookie!
-    #   return ENV['ESHELF_LOGIN_URL']
-    # else
-      stored_location_for(resource) || request.env['omniauth.origin'] || signed_in_root_path(resource)
-    # end
+    if ENV['ESHELF_LOGIN_URL'] && !cookies[ESHELF_COOKIE_NAME]
+      session[:_action_before_eshelf_redirect] = (stored_location_for(resource) || request.env['omniauth.origin'])
+      create_eshelf_cookie!
+      return ENV['ESHELF_LOGIN_URL']
+    else
+      super(resource)
+    end
   end
 
   def after_omniauth_failure_path_for(scope)
