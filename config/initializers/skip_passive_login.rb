17a9c4380f03970cb2629cfb624fde89c23ce411
B Alter <barnaby.alter@gmail.com>
2015-09-14 18:30:33 -0400

    Change name of passive filter in initializer for dev

--- config/initializers/skip_passive_login.rb
+++ config/initializers/skip_passive_login.rb
@@ -1,3 +1,3 @@
 ApplicationController.class_eval do
-  skip_before_filter :check_passive_shibboleth_and_sign_in, if: -> { Rails.env.development? }
+  skip_before_filter :shibboleth_passive_login_check, if: -> { Rails.env.development? }
 end
