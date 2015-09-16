143eefb2f0ed0056d9623e28e6a2e7642fd737b6
B Alter <barnaby.alter@gmail.com>
2015-09-16 11:42:53 -0400

    If logged out during session, never check passive shibboleth again during tha tsession

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,6 +7,10 @@ class Users::SessionsController < Devise::SessionsController
   # Single sign out by clearing cookies on all sub domains
   def sso_logout
     cookies.clear(domain: ENV['LOGIN_COOKIE_DOMAIN'])
+    # If a user logs into client application and then logs out of that application
+    # and then tries to login to another SSO client application, passive shibboleth
+    # will be called and log them in automatically, this stops that
+    session[:_check_passive_shibboleth] = true
   end
 
   # Save use info in cookies before logging out so we can use them
