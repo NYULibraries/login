48bac39aa2ef3405bb345cb2296db2d6d5e7e308
B Alter <barnaby.alter@gmail.com>
2015-10-14 15:14:08 -0400

    Allow flat file lookup to lookup by identifier or barcode so that users who login with their barcode can access EZBorrow

--- lib/login/aleph/name.rb
+++ lib/login/aleph/name.rb
@@ -1,7 +1,7 @@
 module Login
   module Aleph
     class Name
-      attr_accessor :first_name, :last_name, :fullname, :default_value
+      attr_accessor :fullname, :default_value
 
       # First name has to have some value for PDS to work
       # so either parse the actual name or use the default_value,
@@ -12,25 +12,21 @@ module Login
         end
         @fullname = fullname
         @default_value = default_value
-        @last_name = get_last_name
-        @first_name = get_first_name
       end
 
       ##
       # Extract the firstname from an array of the name split by comma
       # Given name = 'SMITH, JOHN', then names_array.last === first_name
-      def get_first_name
-        @get_first_name ||= (names_array.last.present?) ? names_array.last : default_value
+      def first_name
+        @first_name ||= (names_array.last.present?) ? names_array.last : default_value
       end
-      private :get_first_name
 
       ##
       # Extract the lastname from an array of the name split by comma
       # Given name = 'SMITH, JOHN', then names_array.first === last_name
-      def get_last_name
-        @get_last_name ||= clean_name(names_array.first)
+      def last_name
+        @last_name ||= clean_name(names_array.first)
       end
-      private :get_last_name
 
       # Split name from format 'SMITH, JOHN' to array ['SMITH','JON']
       # Also strip whitespace
