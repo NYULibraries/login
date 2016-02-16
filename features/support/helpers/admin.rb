d52e20e6a9b12b989e84d5e7b00c8fcd884c9948
B Alter <barnaby.alter@gmail.com>
2016-02-16 16:24:39 -0500

    Updated some cukes helpers to shut off warnings

--- features/support/helpers/admin.rb
+++ features/support/helpers/admin.rb
@@ -1,9 +1,8 @@
 module LoginFeatures
   module Admin
-    Provider = FactoryGirl.attributes_for(:admin)[:provider]
     def set_admin_login_env
       set_admin_account
-      OmniAuth.config.mock_auth[Provider.to_sym] = admin_omniauth_hash
+      OmniAuth.config.mock_auth[admin_provider.to_sym] = admin_omniauth_hash
     end
 
     def omniauth_hash_map
@@ -11,16 +10,20 @@ module LoginFeatures
     end
 
     def admin_omniauth_hash
-      @admin_omniauth_hash ||= OmniAuth::AuthHash.new(FactoryGirl.create("#{Provider}_authhash".to_sym))
+      @admin_omniauth_hash ||= OmniAuth::AuthHash.new(FactoryGirl.create("#{admin_provider}_authhash".to_sym))
     end
 
     def login_as_admin
-      follow_login_steps_for_location provider_to_location[Provider]
+      follow_login_steps_for_location provider_to_location[admin_provider]
     end
 
     def set_admin_account
       FactoryGirl.create(:admin, omniauth_hash_map: omniauth_hash_map, username: omniauth_hash_map.username)
     end
     private :set_admin_account
+
+    def admin_provider
+      @admin_provider ||= FactoryGirl.attributes_for(:admin)[:provider]
+    end
   end
 end
