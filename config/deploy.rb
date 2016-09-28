4eac3d3f3e0792fe56f5ffbf8caa9a52bc63bf3f
Barnaby Alter <barnaby.alter@nyu.edu>
2016-09-28 15:03:34 -0400

    Updated ruby to 2.3.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,4 +2,4 @@ require 'formaggio/capistrano'
 set :recipient, "lib-webservices@nyu.edu"
 set :app_title, "login"
 set :keep_releases, 5
-set :rvm_ruby_string, "2.1.3"
+set :rvm_ruby_string, "2.3.1"
