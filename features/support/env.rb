fb78b43d33e01d7a42c0c20c3b9ba8e84bf0f0b9
Eric Griffis <eric.griffis@nyu.edu>
2017-04-11 15:52:53 -0400

    Add back RAILS_ENV override in cucumber due to devise env-based config

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
-ENV['RAILS_ENV'] ||= 'cucumber'
+ENV['RAILS_ENV'] = 'cucumber'
 
 require 'cucumber/rails'
 # Include these helpers so we can stub the IP addresses
