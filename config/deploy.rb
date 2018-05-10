4090ea998df47f49211f78f687fe63a47c6ff5eb
BAlter <barnaby.alter@nyu.edu>
2018-05-10 15:00:35 -0400

    Trick formaggio into not deploying newrelic

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,4 @@ set :app_title, "login"
 set :keep_releases, 5
 set :rvm_ruby_string, "2.3.4"
 set :assets_gem, ["nyulibraries_stylesheets.git", "nyulibraries_javascripts.git"]
+set :new_relic_environments, ["none"]
