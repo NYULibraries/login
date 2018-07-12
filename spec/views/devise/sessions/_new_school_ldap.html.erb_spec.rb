8db58b22ff5a5fbf8d932af4769c1e584441c78b
BAlter <barnaby.alter@nyu.edu>
2018-07-12 15:43:51 -0400

    Changed spec to match new markup

--- spec/views/devise/sessions/_new_school_ldap.html.erb_spec.rb
+++ spec/views/devise/sessions/_new_school_ldap.html.erb_spec.rb
@@ -7,7 +7,7 @@ describe "devise/sessions/_new_school_ldap.html.erb" do
     should match(/<form/)
     should match('method="post"')
     should match(/action="\/users\/auth\/new_school_ldap\/callback\?institution=/)
-    should match(/<legend class="heading">\s*Login with a New School NetID/)
+    should match(/<legend>\s*Login with a New School NetID/)
     should match('<label for="username">Enter your NetID</label>')
     should match('<input type="text" name="username" id="username"')
     should match('<label for="password">Enter your password</label>')
