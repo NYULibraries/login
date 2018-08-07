04300276a62dbf275668975c9ec8e07b490d27c3
Elliott Grieco <elliott.grieco@nyu.edu>
2018-08-07 16:55:36 -0400

    Completes 2.3.4 change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,6 @@ require 'formaggio/capistrano'
 set :recipient, "lib-webservices@nyu.edu"
 set :app_title, "login"
 set :keep_releases, 5
-set :rvm_ruby_string, "2.5.1"
+set :rvm_ruby_string, "2.3.4"
 set :assets_gem, ["nyulibraries_stylesheets.git", "nyulibraries_javascripts.git"]
 set :new_relic_environments, ["none"]
