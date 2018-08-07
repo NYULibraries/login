5efed21c0b720fe5261402d496c20c195df7dbb8
Elliott Grieco <elliott.grieco@nyu.edu>
2018-08-07 13:59:01 -0400

    Small configurations cleanups

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Do not force ssl in development.
+  config.force_ssl = false
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
