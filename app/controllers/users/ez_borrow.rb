4436290d72dc704d39317f44ea00bb1424b1da58
Elliott Grieco <elliott.grieco@nyu.edu>
2018-08-10 13:42:43 -0400

    Improves ezborrow controller module

--- app/controllers/users/ez_borrow.rb
+++ app/controllers/users/ez_borrow.rb
@@ -1,7 +1,15 @@
 module Users
   module EZBorrow
-    UNAUTHORIZED_REDIRECT = "https://library.nyu.edu/errors/ezborrow-library-nyu-edu/unauthorized".freeze
+    UNAUTHORIZED_REDIRECT = <<~HEREDOC.freeze
+      https://library.nyu.edu/errors/ezborrow-library-nyu-edu/unauthorized
+    HEREDOC
+
     URL_BASE = "https://e-zborrow.relaisd2d.com/service-proxy/".freeze
+    AUTHORIZED_INSTITUTIONS = %w(nyu nyush nyuad).freeze
+
+    def self.included(base)
+      base.prepend_before_action :verify_institution, raise: false
+    end
 
     def ezborrow
       ezborrow_user = Login::EZBorrow.new(current_user)
@@ -14,5 +22,21 @@ module Users
         redirect_to UNAUTHORIZED_REDIRECT
       end
     end
+
+    def verify_institution
+      if invalid_institution?
+        flash[:alert] = <<~HEREDOC.squeeze
+          EZBorrow priviledges are only eligible to eligible NYU borrowers.
+          Please log in to confirm your EZBorrow priviledge eligibility.
+        HEREDOC
+        redirect_to login_path('nyu')
+      end
+    end
+    private :verify_institution
+
+    def invalid_institution?
+      AUTHORIZED_INSTITUTIONS.include?(params[:institution])
+    end
+    private :invalid_institution
   end
 end
