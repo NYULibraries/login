f815b9743e929067ea644bd51c1a6af93c63f89c
BAlter <barnaby.alter@nyu.edu>
2018-09-10 12:27:40 -0400

    Change deploys kept from 5 to 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'formaggio/capistrano'
 set :recipient, "lib-webservices@nyu.edu"
 set :app_title, "login"
-set :keep_releases, 5
+set :keep_releases, 3
 set :rvm_ruby_string, "2.5.1"
 set :assets_gem, ["nyulibraries_stylesheets.git", "nyulibraries_javascripts.git"]
 set :new_relic_environments, ["none"]
