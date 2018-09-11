33635410498d3f33d16348917fe65ebfc6270108
Elliott Grieco <elliott.grieco@nyu.edu>
2018-09-11 17:19:41 -0400

    Cleans up docker, controllers, and factory_bot monkey patch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < Devise::OmniauthCallbacksController
-  # prepends so that passthru methods take precedence, and super refers to this controller
-  prepend Users::Passthru
-
   include Users::ClientPassiveLogin
   include Users::EZBorrowLogin
-  include Users::OmniauthProvider
+  include Users::OmniauthCallback
 
   before_action :redirect_root, if: -> { request.path == '/' && user_signed_in? }
   before_action :require_login!, only: [:show, :ezborrow_login]
   before_action :authenticate_user!, only: [:passthru, :client_passive_login]
+  skip_before_action :verify_authenticity_token if Rails.env.development?
   respond_to :html
+
+  ESHELF_COOKIE_NAME = '_nyulibraries_eshelf_passthru'.freeze
 
   def show
     @user = User.find_by(username: params[:id], provider: params[:provider])
@@ -20,8 +20,29 @@ class UsersController < Devise::OmniauthCallbacksController
     end
   end
 
+  # GET /passthru
+  # Redirect to original stored location after being sent back to the Login app
+  # from the eshelf login
+  def passthru
+    # Assuming we authenticated in E-shelf
+    # Return to the original location, on active login
+    # or back to the passive login action, on passive
+    action_before_eshelf_redirect = session[:_action_before_eshelf_redirect]
+    session[:_action_before_eshelf_redirect] = nil
+    cookies.delete(ESHELF_COOKIE_NAME, domain: ENV['LOGIN_COOKIE_DOMAIN'])
+    redirect_to stored_location_for(:user) || action_before_eshelf_redirect || signed_in_root_path(:user)
+  end
+
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || request.env['omniauth.origin'] || super(resource)
+    # If there is an eshelf login variable set then we want to redirect there after login
+    # to permanently save eshelf records
+    if ENV['ESHELF_LOGIN_URL'] && !cookies[ESHELF_COOKIE_NAME]
+      session[:_action_before_eshelf_redirect] = (stored_location_for(resource) || request.env['omniauth.origin'])
+      create_eshelf_cookie!
+      return ENV['ESHELF_LOGIN_URL']
+    else
+      super(resource)
+    end
   end
 
   private
@@ -46,5 +67,10 @@ class UsersController < Devise::OmniauthCallbacksController
 
   def bobcat_institutions
     @bobcat_institutions ||= Login::Aleph::Patron::BOR_STATUS_MAPPINGS.keys
+  end
+
+  def create_eshelf_cookie!
+    cookie_hash = { value: 1, httponly: true, domain: ENV['LOGIN_COOKIE_DOMAIN'] }
+    cookies[ESHELF_COOKIE_NAME] = cookie_hash
   end
 end
