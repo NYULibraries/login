f744a6cf29fd2706fce3efa5c8317bc0d879fb1d
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-09 17:15:32 -0400

    Fixes configs for circle

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -e
 
 : "${ECR_DOMAIN?Must specify ECR_DOMAIN}"
-IMAGE_NAME=login
 
-docker tag $IMAGE_NAME $ECR_DOMAIN/$IMAGE_NAME:latest
-docker push $ECR_DOMAIN/$IMAGE_NAME:latest
-docker tag $IMAGE_NAME $ECR_DOMAIN/$IMAGE_NAME:${CIRCLE_BRANCH//\//_}
-docker push $ECR_DOMAIN/$IMAGE_NAME:${CIRCLE_BRANCH//\//_}
-docker tag $IMAGE_NAME $ECR_DOMAIN/$IMAGE_NAME:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
-docker push $ECR_DOMAIN/$IMAGE_NAME:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker tag $IMAGE_NAME $ECR_DOMAIN/login:latest
+docker push $ECR_DOMAIN/login:latest
+docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
+docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
+docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+
+docker tag $IMAGE_NAME $ECR_DOMAIN/login_test:latest
+docker push $ECR_DOMAIN/login_test:latest
+docker tag login_test $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
+docker push $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
+docker tag login_test $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker push $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
