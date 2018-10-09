724cc1be9dd05a4c8ca76f7104c8dfcc678ba8a5
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-09 17:32:24 -0400

    Fixes circle push docker

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -2,14 +2,14 @@
 
 : "${ECR_DOMAIN?Must specify ECR_DOMAIN}"
 
-docker tag $IMAGE_NAME $ECR_DOMAIN/login:latest
+docker tag login $ECR_DOMAIN/login:latest
 docker push $ECR_DOMAIN/login:latest
 docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
 docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
 docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker tag $IMAGE_NAME $ECR_DOMAIN/login_test:latest
+docker tag login_test $ECR_DOMAIN/login_test:latest
 docker push $ECR_DOMAIN/login_test:latest
 docker tag login_test $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
 docker push $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
