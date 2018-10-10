300ffe6e7ed7b841b5df1a05e0938caabde59394
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-10 12:40:23 -0400

    Fix docker push procedure

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -2,16 +2,17 @@
 
 : "${ECR_DOMAIN?Must specify ECR_DOMAIN}"
 
-docker tag login $ECR_DOMAIN/login:latest
-docker push $ECR_DOMAIN/login:latest
-docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
-docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}
-docker tag login $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
-docker push $ECR_DOMAIN/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+tag=login
+subtags="test unicorn"
 
-docker tag login_test $ECR_DOMAIN/login_test:latest
-docker push $ECR_DOMAIN/login_test:latest
-docker tag login_test $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
-docker push $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}
-docker tag login_test $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
-docker push $ECR_DOMAIN/login_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+for subtag in $subtags
+  docker tag $tag_$subtag $ECR_DOMAIN/$tag_$subtag:latest
+  docker tag $tag_$subtag $ECR_DOMAIN/$tag_$subtag:${CIRCLE_BRANCH//\//_}
+  docker tag $tag_$subtag $ECR_DOMAIN/$tag_$subtag:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+done
+
+for subtag in $subtags
+  docker push $ECR_DOMAIN/$tag_$subtag:latest
+  docker push $ECR_DOMAIN/$tag_$subtag:${CIRCLE_BRANCH//\//_}
+  docker push $ECR_DOMAIN/$tag_$subtag:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+done
