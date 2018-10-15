c14f0e8092346bdd175ce4cce89a0f25f64353ed
Elliott Grieco <elliott.grieco@nyu.edu>
2018-10-15 16:54:15 -0400

    Pulls appropriate docker images from ECR

--- script/docker/pull_docker.sh
+++ script/docker/pull_docker.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
 
 : "${ECR_DOMAIN?Must specify ECR_DOMAIN}"
-IMAGE_NAME=login
 
-# check if branch tag exists
-aws ecr describe-images --region us-east-1 --repository-name=login --image-ids=imageTag=${CIRCLE_BRANCH//\//_}
+subtags="test unicorn"
 
-if [ $? -eq 0 ]; then
-  docker pull $ECR_DOMAIN/$IMAGE_NAME:${CIRCLE_BRANCH//\//_}
-else
-  docker pull $ECR_DOMAIN/$IMAGE_NAME:latest
-fi
+for subtag in $subtags
+do
+  docker pull $ECR_DOMAIN/login_$subtag:${CIRCLE_BRANCH//\//_} || docker pull $ECR_DOMAIN/login_$subtag:latest
+done
