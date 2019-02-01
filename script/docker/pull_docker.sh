e018eee862533d05ae00e1fd3aa33df2743fb302
Eric Griffis <eric.griffis@nyu.edu>
2019-02-01 14:51:32 -0500

    Circle - push images to quay

--- script/docker/pull_docker.sh
+++ script/docker/pull_docker.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -e
 
-: "${IMAGES_DOMAIN?Must specify IMAGES_DOMAIN}"
-
-docker pull $IMAGES_DOMAIN/login:${CIRCLE_BRANCH//\//_} || docker pull $IMAGES_DOMAIN/login:latest
+docker pull nyulibraries/login:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/login:latest
 
 subtags="test"
 
 for subtag in $subtags
 do
-  docker pull $IMAGES_DOMAIN/login_$subtag:${CIRCLE_BRANCH//\//_} || docker pull $IMAGES_DOMAIN/login_$subtag:latest
+  docker pull nyulibraries/login_$subtag:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/login_$subtag:latest
 done
