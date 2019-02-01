f56a90bb5d82004864e38c2a915e8c718d5351cf
Eric Griffis <eric.griffis@nyu.edu>
2019-02-01 14:57:15 -0500

    Fix docker push script

--- script/docker/push_docker.sh
+++ script/docker/push_docker.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 docker tag login quay.io/nyulibraries/login:latest
 docker tag login quay.io/nyulibraries/login:${CIRCLE_BRANCH//\//_}
 docker tag login quay.io/nyulibraries/login:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
-docker tag login_test quay.io/nyulibraries/login_$subtag:latest
-docker tag login_test quay.io/nyulibraries/login_$subtag:${CIRCLE_BRANCH//\//_}
-docker tag login_test quay.io/nyulibraries/login_$subtag:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker tag login_test quay.io/nyulibraries/login_test:latest
+docker tag login_test quay.io/nyulibraries/login_test:${CIRCLE_BRANCH//\//_}
+docker tag login_test quay.io/nyulibraries/login_test:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
 docker push quay.io/nyulibraries/login:latest
 docker push quay.io/nyulibraries/login:${CIRCLE_BRANCH//\//_}
