9395150a7e890972f40e69fd7b002c0ce690ec57
BAlter <barnaby.alter@nyu.edu>
2019-02-20 11:24:35 -0500

    Create dummy NYUShibboleth2 class

---
+++ lib/login/omni_auth_hash/nyu_shibboleth2.rb
@@ -0,0 +1,22 @@
+##
+# Inherits default values for attributes from base class
+# and overrides NYU Shibboleth specific ones
+module Login
+    module OmniAuthHash
+      module ProviderMapper
+        class NYUShibboleth2 < Base
+          def initialize(omniauth_hash)
+            @omniauth_hash = omniauth_hash
+            @institution_code = "NYU"
+            @nyuidn = omniauth_hash.extra.raw_info.nyuidn
+            super(omniauth_hash)
+          end
+  
+          def properties_attributes
+            super.merge({entitlement: omniauth_hash.extra.raw_info.entitlement})
+          end
+        end
+      end
+    end
+  end
+  
