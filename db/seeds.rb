342a2d4957ff18555429109dbcbfbfb1ff4cf996
Eric Griffis <eric.griffis@nyu.edu>
2020-07-14 16:32:18 -0400

    Add seed for optional admin application credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,16 @@ if Rails.env.development? and User.find_by_username(username).nil?
   })
   user.save!  
 end
+
+admin_client_id = ENV['SEED_ADMIN_CLIENT_ID']
+admin_client_secret = ENV['SEED_ADMIN_CLIENT_SECRET']
+if Rails.env.development? && admin_client_id && admin_client_secret
+  app = Doorkeeper::Application.create!({
+    name: "Seed Admin App",
+    uid: admin_client_id,
+    secret: admin_client_secret,
+    redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+    scopes: ["admin"],
+    confidential: true,
+  })
+end
