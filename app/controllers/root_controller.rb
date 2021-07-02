2d031b7ef196d55361a17a667fcc7ec027f30c7c
Eric Griffis <eric.griffis@nyu.edu>
2021-07-02 13:14:30 -0400

    Add cache check to healthcheck

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,8 +2,13 @@ class RootController < ApplicationController
   before_action :require_login!, except: [:healthcheck]
 
   def healthcheck
-    render json: {success: true}
+    render json: {success: is_cache_up?}
     return
+  end
+
+  private
+  def is_cache_up?
+    ActionController::Base.new.read_fragment("CACHE_TEST_KEY") == "VALID"
   end
 
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cache_store, key: '_login_session'
+
+ActionController::Base.new.write_fragment("CACHE_TEST_KEY", "VALID")
